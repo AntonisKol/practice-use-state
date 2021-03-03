49a551d2c551f0838cc1bf67f09bfaa9a894afbe
MAC <mac@Antonis.local>
2021-03-03 15:39:49 +0100

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+     const [getValue, setValue] = useState('');
+     let status ="Text is too short";
+
+     if (getValue.length >= 5 ) {
+     status = "Text is ok"
+ }
+      let letters = getValue.split('')
+      let boxes = letters.map(letter =><h3 className="card">{letter}</h3>);
+
+      return (
+      <div className="App">
+      <input 
+      onChange={e => setValue(e.target.value)} value= {getValue}
+      />
+       
+      <h3>{status}</h3>
+      <p> You have typed <span>{getValue.length}</span> characters </p>
+      <div>{boxes}</div>
+      </div> 
+
+    );
+  }
+  export default App;
